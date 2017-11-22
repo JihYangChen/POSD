4385a16a4d8458688ab2231b7aab7640f794ca8d
AndyChen <andy565866@gmail.com>
2017-11-22 15:33:53 +0800

    Refactoring parser.h' interface(createTerm, createTerms, getTerms). Update new utParser's test case.

--- parser.h
+++ parser.h
@@ -25,19 +25,16 @@ public:
         else if (token == NUMBER){
             return new Number(_scanner.tokenValue());
         }
-     
+        
         // Create Atom or Struct
         else if (token == ATOM || token == ATOMSC){
             Atom* atom = new Atom(symtable[_scanner.tokenValue()].first);
             
             // Create Struct
             if(_scanner.nextToken() == '(') {
-                
-                vector<Term*> structArgs = getArgs();
-                if(_scanner.nextToken() != ')')
-                    throw string("unexpected token");
-                return new Struct(*atom, structArgs);
+                return createStructure(*atom);
             }
+            // Create Atom
             else {
                 _scanner.positionBackward();
                 return atom;
@@ -46,39 +43,70 @@ public:
         
         // Create List
         else if (token == '[') {
-            vector<Term *> listArgs = getArgs();
-            if(_scanner.nextToken() != ']')
-                throw string("unexpected token");
-            return new List(listArgs);
+            return createList();
         }
-        
         
         return nullptr;
     }
     
-    // Return a vector of arguments, after calling the right parentheses and square brackets will NOT be discarded(nextToken will be ')' or ']').
-    vector<Term*> getArgs()
+    vector<Term*> getTerms() {
+        return _terms;
+    }
+    
+
+private:
+    FRIEND_TEST(ParserTest, createArgs);
+    FRIEND_TEST(ParserTest,ListOfTermsEmpty);
+    FRIEND_TEST(ParserTest,listofTermsTwoNumber);
+    FRIEND_TEST(ParserTest, createTerm_nestedStruct3);
+    
+    // Create a vector of arguments in _terms, the right parentheses and square brackets will NOT be discarded(nextToken will be ')' or ']') after calling.
+    void createTerms()
     {
-        vector<Term*> args = {};
         Term* term = createTerm();
         if (term) {
-            args.push_back(term);
-            while (_scanner.currentChar() == ',') {
-                _scanner.nextToken();
-                args.push_back(createTerm());
+            _terms.push_back(term);
+            while (_scanner.nextToken() == ',') {
+                _terms.push_back(createTerm());
             }
+            
+            // Right parentheses
+            _scanner.positionBackward();
         }
         // Empty Struct or List
         else {
             _scanner.positionBackward();
         }
-        
-        return args;
     }
     
+    Term * createStructure(Atom & structName) {
+        int startIndexOfStructArgs = (int)_terms.size();
+        createTerms();
+        
+        if(_scanner.nextToken() != ')')
+            throw string("unexpected token");
+        
+        vector<Term *> args(_terms.begin() + startIndexOfStructArgs, _terms.end());
+        _terms.erase(_terms.begin() + startIndexOfStructArgs, _terms.end());
+        
+        return new Struct(structName, args);
+    }
     
+    Term * createList() {
+        int startIndexOfListArgs = (int)_terms.size();
+        createTerms();
+        
+        if(_scanner.nextToken() != ']')
+            throw string("unexpected token");
+        
+        vector<Term *> args(_terms.begin() + startIndexOfListArgs, _terms.end());
+        _terms.erase(_terms.begin() + startIndexOfListArgs, _terms.end());
+        
+        return new List(args);
+    }
     
-private:
     Scanner _scanner;
+    vector<Term *> _terms;
 };
 #endif
+
